e58a352f375398b761568df7f909cd488f32d754
fawaz <FawazHutomiA>
2022-02-10 07:16:09 +0700

    modal

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,50 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="center">
+    <h2>Click this button bellow to show Modal</h2>
+    <button id="myBtn" @click="openModal">Open Modal</button>
+  </div>
+  <Modal v-if="isModalOpen" @close-modal="closeModal" :payload="modalData" />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import { onMounted, reactive, ref } from "vue";
+import Modal from "./components/Modal.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Modal,
+  },
+  setup() {
+    const isModalOpen = ref(false);
+    const modalData = reactive({
+      modal_header: "Modal Header", 
+      modal_content: 100,
+      modal_footer: "Modal Footer"
+    })
+
+    onMounted(() => {
+      window.addEventListener("keydown", function (e) {
+        if (e.key === "Escape") {
+          isModalOpen.value = false;
+        }
+      });
+    });
+
+    function openModal() {
+      isModalOpen.value = true;
+    }
+
+    function closeModal() {
+      isModalOpen.value = false;
+    }
+
+    return {
+      isModalOpen,
+      modalData,
+      openModal,
+      closeModal,
+    };
   },
 };
 </script>
@@ -22,5 +57,22 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+div button {
+  font-size: 17px;
+  background-color: #008cba;
+  border: none;
+  padding: 15px 32px;
+  cursor: pointer;
+  color: white;
+  border-radius: 4px;
+  transition-duration: 0.4s;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.4);
+}
+
+div button:hover {
+  box-shadow: 0 12px 16px 0 rgba(0, 0, 0, 0.24),
+    0 17px 50px 0 rgba(0, 0, 0, 0.19);
 }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,124 @@
+<template>
+  <div id="myModal" class="modal" @click="closeModal">
+    <div class="modal-content">
+      <div class="modal-header">
+        <span class="close" @click="closeModal">&times;</span>
+        <h2>{{ payload.modal_header }}</h2>
+      </div>
+      <div class="modal-body">
+        <div v-for="item in payload.modal_content" :key="item">
+            <h1> {{ item }}. Fawaz Hutomi A  </h1>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <h3>{{ payload.modal_footer }}</h3>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    payload: {
+      type: Object,
+    },
+  },
+  setup(_, { emit }) {
+    function closeModal() {
+      emit("close-modal");
+    }
+
+    return {
+      closeModal,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.center {
+  text-align: center;
+}
+
+.modal {
+  display: block;
+  position: fixed;
+  z-index: 1;
+  padding-top: 100px;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.4);
+}
+
+.modal-content {
+  position: relative;
+  background-color: #fefefe;
+  margin: auto;
+  padding: 0;
+  border: none;
+  width: 60%;
+  -webkit-animation-name: animatetop;
+  -webkit-animation-duration: 0.4s;
+  animation-name: animatetop;
+  animation-duration: 0.4s;
+}
+
+@-webkit-keyframes animatetop {
+  from {
+    top: -100px;
+    opacity: 0;
+  }
+  to {
+    top: 0;
+    opacity: 1;
+  }
+}
+
+@keyframes animatetop {
+  from {
+    top: -100px;
+    opacity: 0;
+  }
+  to {
+    top: 0;
+    opacity: 1;
+  }
+}
+
+.close {
+  color: white;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+  color: #000;
+  text-decoration: none;
+  cursor: pointer;
+}
+
+.modal-header {
+  padding: 2px 16px;
+  background-color: #008cba;
+  color: white;
+  text-align: center;
+}
+
+.modal-body {
+  padding: 2px 16px;
+  height: 300px;
+  overflow: auto;
+}
+
+.modal-footer {
+  padding: 2px 16px;
+  background-color: #008cba;
+  color: white;
+  text-align: center;
+}
+</style>
